279830380492933d0f3d2db67fd9b969be87c3ed
Mona Aloufi <aloufihmona@gmail.com>
2025-02-16 20:27:24 +0300

    login and dashboard pages

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,8 +3,9 @@ from . import views
 from django.contrib.auth import views as auth_views
 urlpatterns = [
  path('', views.home),
- path('', views.login, name='login'),
-
-
+ path('en/login/',views.login_view, name='login'),
+ path('en/signup/',views.signin_view, name='signup'),
+ path('dashboard',views.dashboard, name='dashboard'),
+  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 def home(request):
  return render(request, "Blay/home.html") 
-def login(request):
- return render(request, "login.html") 
+def login_view(request):
+    return render(request, 'login.html')
+def signin_view(request):
+    return render(request, 'signin.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
 
