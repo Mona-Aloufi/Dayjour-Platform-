cbfebfe88f0c835bbad8b91c5950cbc1f7a3494a
Mona Aloufi <aloufihmona@gmail.com>
2025-02-17 01:53:52 +0300

    upload Data and Train model pages

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-urlpatterns = [
- path('', views.home),
- path('en/login/',views.login_view, name='login'),
- path('en/signup/',views.signin_view, name='signup'),
- path('dashboard',views.dashboard, name='dashboard'),
-  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('en/login/', views.login_view, name='login'),
+    path('en/signup/', views.signin_view, name='signup'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('upload/', views.encrypt_data_view, name='upload_data'),
+    path('download/<str:encrypted_file>/', views.download_encrypted_file_view, name='download_encrypted_file'),
+     path('trainLocalmodel/', views.trainLocalmodel, name='train_local_model'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,12 +1,46 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
 
 def home(request):
- return render(request, "Blay/home.html") 
+    return render(request, "Blay/home.html")
+
 def login_view(request):
     return render(request, 'login.html')
+
 def signin_view(request):
     return render(request, 'signin.html')
 
 def dashboard(request):
     return render(request, 'dashboard.html')
 
+def encrypt_data_view(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['dataFile']
+        
+        # Simulate saving the uploaded file
+        saved_file_path = os.path.join('path/to/save/uploaded/files', uploaded_file.name)
+        
+        # Save the uploaded file to the filesystem
+        with open(saved_file_path, 'wb+') as destination:
+            for chunk in uploaded_file.chunks():
+                destination.write(chunk)
+
+        # Show the download page with the file name
+        return render(request, 'download_encrypt.html', {'encrypted_file': uploaded_file.name})
+
+    return render(request, 'uploadData.html')
+
+def download_encrypted_file_view(request, encrypted_file):
+    file_path = os.path.join('path/to/save/uploaded/files', encrypted_file)
+    
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as f:
+            response = HttpResponse(f.read(), content_type='application/octet-stream')
+            response['Content-Disposition'] = f'attachment; filename="{encrypted_file}"'
+            return response
+
+    return HttpResponse("File not found.", status=404)
+
+def trainLocalmodel(request):
+     return render(request, 'train.html')
